f537dae4d0b0fc91d060a3c69c960c9c9ef8ee39
Brecht Bonte <nbz_tricksta@hotmail.com>
2014-10-28 16:10:49 +0100

    added tests for large ordered lists

--- test/ListItemsTest.php
+++ test/ListItemsTest.php
@@ -50,4 +50,43 @@ EOT;
 
         $this->assertEquals($expected_output, $output);
     }
+
+    public function testLargeOrderedList()
+    {
+        $input = <<<EOT
+<ol>
+    <li>one</li>
+    <li>two</li>
+    <li>three</li>
+    <li>four</li>
+    <li>five</li>
+    <li>six</li>
+    <li>seven</li>
+    <li>eight</li>
+    <li>nine</li>
+    <li>ten</li>
+    <li>eleven</li>
+</ol>
+EOT;
+        $expected_output =<<<EOT
+ 	 1. one
+ 	 2. two
+ 	 3. three
+ 	 4. four
+ 	 5. five
+ 	 6. six
+ 	 7. seven
+ 	 8. eight
+ 	 9. nine
+ 	10. ten
+ 	11. eleven
+
+
+EOT;
+
+        $html2text = new \Html2Text\Html2Text($input);
+        $output = $html2text->get_text();
+
+        $this->assertEquals($expected_output, $output);
+    }
 }
