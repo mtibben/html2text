3ba9d7a970a8d17c83526cf6ab7829dc2389b11e
Onno Vos <onno.vos@klarna.com>
2014-12-01 18:29:16 +0100

    Add test-case for Table that contains a <td> with spaces

--- test/TableTest.php
+++ test/TableTest.php
@@ -2,6 +2,8 @@
 
 namespace Html2Text;
 
+require_once("C:\\Stash\\Html2TexT_PR\\html2text\\src\\Html2Text.php");
+
 class TableTest extends \PHPUnit_Framework_TestCase
 {
     public function testTable()
@@ -25,6 +27,41 @@ EOT;
 
  		HEADING 2
  		Data 2
+
+
+EOT;
+
+        $html2text = new Html2Text($html);
+        $this->assertEquals($expected, $html2text->getText());
+    }
+
+    public function testTableWithSpaces()
+    {
+        $html =<<<'EOT'
+<table>
+  <tr>
+    <th>Heading 1</th>
+    <td>Data 1a</td>
+    <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    <td>Data 1b</td>
+  </tr>
+  <tr>
+    <th>Heading 2</th>
+    <td>Data 2a</td>
+    <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    <td>Data 2b</td>
+  </tr>
+</table>
+EOT;
+
+        $expected =<<<'EOT'
+ 		HEADING 1
+ 		Data 1a
+ 		Data 1b
+
+ 		HEADING 2
+ 		Data 2a
+ 		Data 2b
 
 
 EOT;
