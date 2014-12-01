fc2908f9a24e07f0088ac1ca2ce96b797e8ae21d
Onno Vos <onno.vos@klarna.com>
2014-12-01 19:00:44 +0100

    Remove PHPDoc Comments and ensure that formatting of the class as a whole is consistent as in all other Tests

--- test/BlankSpacesTest.php
+++ test/BlankSpacesTest.php
@@ -4,10 +4,6 @@ namespace Html2Text;
 
 class BlankSpacesTest extends \PHPUnit_Framework_TestCase
 {
-
-    /**
-     * Test multiple &nbsp; in a row.
-     */
     public function testMultipleBlankSpaces()
     {
         $html =<<<'EOT'
@@ -22,10 +18,6 @@ EOT;
         $this->assertEquals($expected, $html2text->getText());
     }
 
-
-    /**
-     * Test multiple &emsp; in a row.
-     */
     public function testMultipleEmSpaces()
     {
         $html =<<<'EOT'
@@ -40,10 +32,6 @@ EOT;
         $this->assertEquals($expected, $html2text->getText());
     }
 
-
-    /**
-     * Test multiple &ensp; in a row.
-     */
     public function testMultipleEnspaces()
     {
         $html =<<<'EOT'
@@ -58,10 +46,6 @@ EOT;
         $this->assertEquals($expected, $html2text->getText());
     }
 
-
-    /**
-     * Test multiple &thinsp; in a row.
-     */
     public function testMultipleThinspaces()
     {
         $html =<<<'EOT'
@@ -76,10 +60,6 @@ EOT;
         $this->assertEquals($expected, $html2text->getText());
     }
 
-
-    /**
-     * Create a random string containing 2 spaces in a row, followed by one extra space.
-     */
     public function testRandomSpaces()
     {
         $possibleSpaces = array("&nbsp;", "&emsp;", "&ensp;", "&thinsp;");
@@ -94,9 +74,6 @@ EOT;
         $this->assertEquals($expected, $html2text->getText());
     }
 
-    /**
-     * Create a random string containing 2 spaces in a row, followed by another 2 spaces in a row but of a different html entity.
-     */
     public function testRandomSpacesDouble()
     {
         $possibleSpaces = array("&nbsp;", "&emsp;", "&ensp;", "&thinsp;");
@@ -110,6 +87,4 @@ EOT;
         $html2text = new Html2Text($randomSpacesString);
         $this->assertEquals($expected, $html2text->getText());
     }
-
-
 }
