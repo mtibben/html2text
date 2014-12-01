06061b602227e33bb139bdd8e754fa139d8e718e
Onno Vos <onno.vos@klarna.com>
2014-12-01 18:36:59 +0100

    Add additional testcases for each of the different kinds of blank spaces available

--- test/TableTest.php
+++ test/TableTest.php
@@ -68,7 +68,7 @@ EOT;
         $this->assertEquals($expected, $html2text->getText());
     }
 
-    public function testTableWithSpaces()
+    public function testTableWithBlankSpaces()
     {
         $html =<<<'EOT'
 <table>
@@ -82,6 +82,111 @@ EOT;
     <th>Heading 2</th>
     <td>Data 2a</td>
     <td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+    <td>Data 2b</td>
+  </tr>
+</table>
+EOT;
+
+        $expected =<<<'EOT'
+ 		HEADING 1
+ 		Data 1a
+ 		Data 1b
+
+ 		HEADING 2
+ 		Data 2a
+ 		Data 2b
+
+
+EOT;
+
+        $html2text = new Html2Text($html);
+        $this->assertEquals($expected, $html2text->getText());
+    }
+
+    public function testTableWithEmSpaces()
+    {
+        $html =<<<'EOT'
+<table>
+  <tr>
+    <th>Heading 1</th>
+    <td>Data 1a</td>
+    <td>&emsp;&emsp;&emsp;&emsp;</td>
+    <td>Data 1b</td>
+  </tr>
+  <tr>
+    <th>Heading 2</th>
+    <td>Data 2a</td>
+    <td>&emsp;&emsp;&emsp;&emsp;</td>
+    <td>Data 2b</td>
+  </tr>
+</table>
+EOT;
+
+        $expected =<<<'EOT'
+ 		HEADING 1
+ 		Data 1a
+ 		Data 1b
+
+ 		HEADING 2
+ 		Data 2a
+ 		Data 2b
+
+
+EOT;
+
+        $html2text = new Html2Text($html);
+        $this->assertEquals($expected, $html2text->getText());
+    }
+
+    public function testTableWithEnSpaces()
+    {
+        $html =<<<'EOT'
+<table>
+  <tr>
+    <th>Heading 1</th>
+    <td>Data 1a</td>
+    <td>&ensp;&ensp;&ensp;&ensp;</td>
+    <td>Data 1b</td>
+  </tr>
+  <tr>
+    <th>Heading 2</th>
+    <td>Data 2a</td>
+    <td>&ensp;&ensp;&ensp;&ensp;</td>
+    <td>Data 2b</td>
+  </tr>
+</table>
+EOT;
+
+        $expected =<<<'EOT'
+ 		HEADING 1
+ 		Data 1a
+ 		Data 1b
+
+ 		HEADING 2
+ 		Data 2a
+ 		Data 2b
+
+
+EOT;
+
+        $html2text = new Html2Text($html);
+        $this->assertEquals($expected, $html2text->getText());
+    }
+
+    public function testTableWithThinSpaces()
+    {
+        $html =<<<'EOT'
+<table>
+  <tr>
+    <th>Heading 1</th>
+    <td>Data 1a</td>
+    <td>&thinsp;&thinsp;&thinsp;&thinsp;</td>
+    <td>Data 1b</td>
+  </tr>
+  <tr>
+    <th>Heading 2</th>
+    <td>Data 2a</td>
+    <td>&thinsp;&thinsp;&thinsp;&thinsp;</td>
     <td>Data 2b</td>
   </tr>
 </table>
