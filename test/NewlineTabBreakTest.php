3ee4c384cbcc9a40eaae2e23b52a9fb366af1057
Clemens Krack <ckrack@gmail.com>
2015-02-12 13:47:24 +0100

    This is a test demonstrating the differences between: - a single-line non-indented paragraph - an indented paragraph - paragraphs containing a break
    This is a test for #7, but it shows that behaviour is ‚correct‘ @Goram.

---
+++ test/NewlineTabBreakTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Html2Text;
+
+class NewlineTabBreakTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNewlineTabBreak()
+    {
+    $html =<<<EOT
+<p>Will be a line</p>
+<p>Will be 
+a line</p>
+<p>
+    This is some text
+    all on one line
+</p>
+<p>
+    This is some text<br/>
+    with a break in the middle
+ </p>
+<p>This is some text<br/>with a break in the middle but no indent</p>
+EOT;
+        $expected =<<<EOT
+Will be a line 
+
+Will be a line 
+
+ This is some text all on one line 
+
+ This is some text
+ with a break in the middle 
+
+This is some text
+with a break in the middle but no indent
+EOT;
+
+        $html2text = new Html2Text($html);
+        $this->assertEquals($expected, $html2text->getText());
+    }
+}
