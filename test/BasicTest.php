66c131e1d2a20b02a5ea7073ab587cfb7b9d8beb
Andrew Nicols <andrew@nicols.co.uk>
2015-10-08 11:49:38 +0800

    Stop running stripslashes on input text
    HTML content preserves slashes (\), and so should Html2Text when it
    converts content.
    
    Note: This is a potentially breaking change as it will mean that slashes
    are no longer stripped out of content where developers were expecting
    Html2Text to perform their sanitisation.

--- test/BasicTest.php
+++ test/BasicTest.php
@@ -4,10 +4,26 @@ namespace Html2Text;
 
 class BasicTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBasicUsageInReadme()
-    {
-        $html = new Html2Text('Hello, &quot;<b>world</b>&quot;');
+    public function basicDataProvider() {
+        return array(
+            'Usage in Readme' => array(
+                'html'      => 'Hello, &quot;<b>world</b>&quot;',
+                'expected'  => 'Hello, "WORLD"',
+            ),
+            'No stripslashes on HTML content' => array(
+                // HTML content does not escape slashes, therefore nor should we.
+                'html'      => 'Hello, \"<b>world</b>\"',
+                'expected'  => 'Hello, \"WORLD\"',
+            ),
+        );
+    }
 
-        $this->assertEquals('Hello, "WORLD"', $html->getText());
+    /**
+     * @dataProvider basicDataProvider
+     */
+    public function testBasic($html, $expected)
+    {
+        $html = new Html2Text($html);
+        $this->assertEquals($expected, $html->getText());
     }
 }
