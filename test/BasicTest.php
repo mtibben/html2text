9f7d4a65cae70cdda7e608afead40d134c61407f
Andrew Nicols <andrew@nicols.co.uk>
2015-10-08 12:27:50 +0800

    Added some new basic tests
    These tests are courtesy of moodle/moodle.
    
    We have a few others, but these will take longer to convert and not all are
    directly relevant.

--- test/BasicTest.php
+++ test/BasicTest.php
@@ -4,10 +4,25 @@ namespace Html2Text;
 
 class BasicTest extends \PHPUnit_Framework_TestCase
 {
-    public function testBasicUsageInReadme()
-    {
-        $html = new Html2Text('Hello, &quot;<b>world</b>&quot;');
+    public function basicDataProvider() {
+        return array(
+            'Readme usage' => array(
+                'html'      => 'Hello, &quot;<b>world</b>&quot;',
+                'expected'  => 'Hello, "WORLD"',
+            ),
+            'Zero is not empty' => array(
+                'html'      => '0',
+                'expected'  => '0',
+            ),
+        );
+    }
 
-        $this->assertEquals('Hello, "WORLD"', $html->getText());
+    /**
+     * @dataProvider basicDataProvider
+     */
+    public function testBasic($html, $expected)
+    {
+        $html = new Html2Text($html);
+        $this->assertEquals($expected, $html->getText());
     }
 }
