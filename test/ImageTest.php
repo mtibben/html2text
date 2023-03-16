81999a8869af275f2533d16bec47e817758f344d
hamza221 <hamzamahjoubi221@gmail.com>
2023-03-16 12:43:27 +0100

    add option to hide alt text

--- test/ImageTest.php
+++ test/ImageTest.php
@@ -35,12 +35,52 @@ class ImageTest extends TestCase
         );
     }
 
+    public function ImageDataWithoutAltProvider() {
+        return array(
+            'Without alt tag' => array(
+                'html' => '<img src="http://example.com/example.jpg">',
+                'expected'  => '',
+            ),
+            'Without alt tag, wrapped in text' => array(
+                'html' => 'xx<img src="http://example.com/example.jpg">xx',
+                'expected'  => 'xxxx',
+            ),
+            'With alt tag' => array(
+                'html' => '<img src="http://example.com/example.jpg" alt="An example image">',
+                'expected'  => '',
+            ),
+            'With alt, and title tags' => array(
+                'html' => '<img src="http://example.com/example.jpg" alt="An example image" title="Should be ignored">',
+                'expected'  => '',
+            ),
+            'With alt tag, wrapped in text' => array(
+                'html' => 'xx<img src="http://example.com/example.jpg" alt="An example image">xx',
+                'expected'  => 'xxxx',
+            ),
+            'With italics' => array(
+                'html' => '<img src="shrek.jpg" alt="the ogrelord" /> Blah <i>blah</i> blah',
+                'expected' => ' Blah _blah_ blah'
+            )
+        );
+    }
+
     /**
      * @dataProvider imageDataProvider
      */
-    public function testImages($html, $expected)
+    public function testImagesAlt($html, $expected)
     {
         $html2text = new Html2Text($html);
+        $output = $html2text->getText();
+
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * @dataProvider ImageDataWithoutAltProvider
+     */
+    public function testImagesNoAlt($html, $expected)
+    {
+        $html2text = new Html2Text($html,array('images' => 'none'));
         $output = $html2text->getText();
 
         $this->assertEquals($expected, $output);
